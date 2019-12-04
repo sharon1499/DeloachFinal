4e0d19e0e2414b09189f4a547f34697e95776d3c
Sharon Deloach <deloach_sharon@columbusstate.edu>
2019-12-04 00:44:28 +0000

    attempt1

---
+++ app.js
@@ -0,0 +1,32 @@
+var express = require("express");
+var fetch = require("node-fetch");
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+
+var title = "";
+var date = "";
+var explanation = "";
+var url = "";
+
+app.get('/', function(req, res){
+    Info();
+    res.render("index",{title:title, date:date, explanation:explanation, url:url});
+});
+
+function getData(){
+    fetch("https://csuserversidewebdevfinal.herokuapp.com/")
+        .then(res => json)
+        .then(json =>{
+            title = json.title;
+            url = json.url;
+            explanation = json.explanation;
+            date = json.date;
+        })
+    };
+
+app.listen(port, function(){
+
+});
